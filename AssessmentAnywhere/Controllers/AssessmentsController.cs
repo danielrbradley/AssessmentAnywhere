dcc5df3ea4983fab3b13b634d1e419d2fd519db1
Daniel Bradley <daniel@danielbradley.net>
2013-03-14 14:35:37 +0000

    Allow result to be set on adding a candidate
    Add more validation of results.
    TODO: add display of validation failures.

--- AssessmentAnywhere/Controllers/AssessmentsController.cs
+++ AssessmentAnywhere/Controllers/AssessmentsController.cs
@@ -1,6 +1,7 @@
 ﻿namespace AssessmentAnywhere.Controllers
 {
     using System;
+    using System.Collections.Generic;
     using System.Linq;
     using System.Web.Mvc;
 
@@ -85,19 +86,63 @@
         [HttpPost]
         public ActionResult Update(Guid id, UpdateModel model)
         {
+            if (model.NewRow == null) model.NewRow = new UpdateResultRow();
+            if (model.Results == null) model.Results = new List<UpdateResultRow>();
+
             var assessment = this.assessmentsRepo.Open(id);
 
             // Validate
             if (!string.IsNullOrWhiteSpace(model.NewRow.Surname)
-                || !string.IsNullOrWhiteSpace(model.NewRow.Forenames))
+                || !string.IsNullOrWhiteSpace(model.NewRow.Forenames)
+                || model.NewRow.Result.HasValue)
             {
                 if (string.IsNullOrWhiteSpace(model.NewRow.Surname))
                 {
-                    ModelState.AddModelError("NewRow.Surname", "Surname cannot be blank");
+                    ModelState.AddModelError("NewRow.Surname", "Surname cannot be blank.");
                 }
                 else if (string.IsNullOrWhiteSpace(model.NewRow.Forenames))
                 {
-                    ModelState.AddModelError("NewRow.Forenames", "Forenames cannot be blank");
+                    ModelState.AddModelError("NewRow.Forenames", "Forenames cannot be blank.");
+                }
+
+                if (model.NewRow.Result.HasValue)
+                {
+                    if (model.NewRow.Result < 0)
+                    {
+                        ModelState.AddModelError("NewRow.Result", "Result must be a positive number.");
+                    }
+
+                    if (model.TotalMarks.HasValue && model.NewRow.Result > model.TotalMarks)
+                    {
+                        ModelState.AddModelError("NewRow.Result", "Result cannot be greater than the total marks.");
+                    }
+                }
+            }
+
+            foreach (var result in model.Results)
+            {
+                var keyPrefix = string.Format("Results[{0}].", model.Results.IndexOf(result));
+                if (string.IsNullOrWhiteSpace(result.Surname))
+                {
+                    ModelState.AddModelError(keyPrefix + "Surname", "Surname cannot be blank.");
+                }
+
+                if (string.IsNullOrWhiteSpace(result.Forenames))
+                {
+                    ModelState.AddModelError(keyPrefix + "Forenames", "Forenames cannot be blank.");
+                }
+
+                if (result.Result.HasValue)
+                {
+                    if (result.Result < 0)
+                    {
+                        ModelState.AddModelError(keyPrefix + "Result", "Result must be a positive number.");
+                    }
+
+                    if (model.TotalMarks.HasValue && result.Result > model.TotalMarks)
+                    {
+                        ModelState.AddModelError(keyPrefix + "Result", "Result cannot be greater than the total marks.");
+                    }
                 }
             }
 
@@ -125,7 +170,7 @@
             int? lastSelectedResult = null;
             if (model.Results != null)
             {
-                foreach (var modelResult in model.Results.Where(r => r.RowId != Guid.Empty))
+                foreach (var modelResult in model.Results)
                 {
                     var assessmentResult = assessment.Results.Single(r => r.Id == modelResult.RowId);
 
@@ -146,7 +191,11 @@
             // Check for new row
             if (!string.IsNullOrWhiteSpace(model.NewRow.Surname) && !string.IsNullOrWhiteSpace(model.NewRow.Forenames))
             {
-                assessment.AddCandidate(model.NewRow.Surname, model.NewRow.Forenames);
+                var newCandidateId = assessment.AddCandidate(model.NewRow.Surname, model.NewRow.Forenames);
+                if (model.NewRow.Result.HasValue)
+                {
+                    assessment.SetCandidateResult(newCandidateId, model.NewRow.Result.Value);
+                }
             }
 
             return this.RedirectToAction("Edit", new { id, lastSelectedResult });
