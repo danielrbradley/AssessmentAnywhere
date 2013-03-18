e40bb7d200a9832ac2bf8e6857d3d4834e8bbee2
Daniel Bradley <daniel@danielbradley.net>
2013-03-18 19:00:40 +0000

    Add simple console test of excel importing

---
+++ AssessmentAnywhere.ExcelTests/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace AssessmentAnywhere.ExcelTests
+{
+    using System.IO;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var stream = File.OpenRead("Test.xlsx"))
+            {
+                var result = AssessmentAnywhere.Excel.AssessmentParser.Parse(stream, 1, "A", "B", "C", 1);
+            }
+        }
+    }
+}
