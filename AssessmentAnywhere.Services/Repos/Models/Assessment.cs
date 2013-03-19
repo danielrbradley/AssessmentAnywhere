be1b51878f0227c77aa60e21aed1561294d1cd53
Daniel Bradley <daniel@danielbradley.net>
2013-03-19 15:44:35 +0000

    Add validation to the assessment domain model

--- AssessmentAnywhere.Services/Repos/Models/Assessment.cs
+++ AssessmentAnywhere.Services/Repos/Models/Assessment.cs
@@ -42,31 +42,95 @@
 
         public Guid AddCandidate(string surname, string forenames)
         {
+            if (string.IsNullOrWhiteSpace(surname))
+            {
+                throw new ArgumentException("Surname must have a value.", "surname");
+            }
+
+            if (string.IsNullOrWhiteSpace(forenames))
+            {
+                throw new ArgumentException("Forenames must have a value.", "forenames");
+            }
+
             var assessmentResult = new AssessmentResult(surname, forenames);
+            if (this.results.Contains(assessmentResult))
+            {
+                throw new UniqueConstraintException(string.Format("A candidate with name \"{0}, {1}\" alread exists.", surname, forenames));
+            }
+
             this.results.Add(assessmentResult);
             return assessmentResult.Id;
         }
 
         public void SetName(string name)
         {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("Name must have a value.", "name");
+            }
+
             this.Name = name;
         }
 
         public void SetTotalMarks(decimal? totalMarks)
         {
+            if (totalMarks.HasValue)
+            {
+                if (totalMarks.Value < 1)
+                {
+                    throw new ArgumentOutOfRangeException("totalMarks", "Total marks value must be greater than zero.");
+                }
+
+                if (results.Any(r => r.Result > totalMarks))
+                {
+                    throw new ArgumentOutOfRangeException(
+                        "totalMarks", "All results must be within than the total marks");
+                }
+            }
+
             this.TotalMarks = totalMarks;
         }
 
         public void SetCandidateNames(Guid id, string surname, string forenames)
         {
+            if (string.IsNullOrWhiteSpace(surname))
+            {
+                throw new ArgumentException("Surname must have a value.", "surname");
+            }
+
+            if (string.IsNullOrWhiteSpace(forenames))
+            {
+                throw new ArgumentException("Forenames must have a value.", "forenames");
+            }
+
             var existingResult = results.Single(r => r.Id == id);
             var newResult = new AssessmentResult(id, surname, forenames, existingResult.Result);
+
+            if (results.Where(r => r.Id != existingResult.Id).Any(r => r.Surname == surname && r.Forenames == forenames))
+            {
+                throw new UniqueConstraintException(string.Format("A candidate with name \"{0}, {1}\" alread exists.", surname, forenames));
+            }
+
             this.results.Remove(existingResult);
             this.results.Add(newResult);
         }
 
         public void SetCandidateResult(Guid id, decimal? result)
         {
+            if (result.HasValue)
+            {
+                if (result < 0)
+                {
+                    throw new ArgumentOutOfRangeException("result", "Result be a positive number.");
+                }
+
+                if (TotalMarks.HasValue && result > TotalMarks)
+                {
+                    throw new ArgumentOutOfRangeException(
+                        "result", "Result must be lower than or equal to the total marks.");
+                }
+            }
+
             var existingResult = results.Single(r => r.Id == id);
             var newResult = new AssessmentResult(id, existingResult.Surname, existingResult.Forenames, result);
             this.results.Remove(existingResult);
---
+++ AssessmentAnywhere.Services/Repos/UniqueConstraintException.cs
@@ -0,0 +1,29 @@
+﻿namespace AssessmentAnywhere.Services.Repos
+{
+    using System;
+    using System.Runtime.Serialization;
+
+    [Serializable]
+    public class UniqueConstraintException : Exception
+    {
+        public UniqueConstraintException()
+        {
+        }
+
+        public UniqueConstraintException(string message)
+            : base(message)
+        {
+        }
+
+        public UniqueConstraintException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+
+        protected UniqueConstraintException(
+            SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
